5c00345d55fc186d960a379c126bad685f85e27c
YJS <90309463+noyyo@users.noreply.github.com>
2024-01-03 21:56:49 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.42 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public string solution(string[] seoul) {
+        const string kim = "Kim";
+        for (int i = 0; i < seoul.Length; i++)
+        {
+            if (seoul[i] == kim)
+            {
+                return $"김서방은 {i}에 있다";
+            }
+        }
+        return "오류";
+    }
+}
