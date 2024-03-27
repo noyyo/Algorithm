b21958f54ea1ee6366fc513d68821e0e8e50dfab
YJS <90309463+noyyo@users.noreply.github.com>
2024-03-27 09:32:39 +0900

    [level 2] Title: 올바른 괄호, Time: 1.34 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public bool solution(string s) {
+        Stack<char> stack = new Stack<char>(s.Length);
+        
+        for (int i = 0; i < s.Length; i++)
+        {
+            
+            if (s[i] == '(')
+                stack.Push(s[i]);
+            else
+            {
+                if (stack.Count == 0)
+                    return false;
+                stack.Pop();
+            }
+        }
+        if (stack.Count > 0)
+            return false;
+        else
+            return true;
+        
+    }
+}
